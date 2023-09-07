953c2d17921ca5f89fc1b7be32ac7d8a39e506b7
Echo De Winter <heyitsecho@protonmail.com>
2023-09-07 15:15:38 +0100

    updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import os
 from flask import Flask, render_template, abort
 from pymongo import MongoClient
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def create_app():
     app = Flask(__name__)
